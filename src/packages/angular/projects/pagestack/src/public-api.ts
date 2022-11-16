ba07aa58b899fe0f682a7f62a5db19aa24669d46
Angel Quinonez <angel27y27@gmail.com>
2022-11-15 23:24:07 -0300

    add service exportation to the public main file

--- src/packages/angular/projects/pagestack/src/public-api.ts
+++ src/packages/angular/projects/pagestack/src/public-api.ts
@@ -1,8 +1,9 @@
 /*
  * Public API Surface of pagestack
  */
+export * from './lib/pagestack.module';
+export * from './lib/services/pagestack.service';
 export * from './lib/components/nav-bar/nav-bar.component';
 export * from './lib/components/nav-bar-item/nav-bar-item.component';
 export * from './lib/components/pages-container/pages-container.component';
 export * from './lib/components/single-page/single-page.component';
-export * from './lib/pagestack.module';
